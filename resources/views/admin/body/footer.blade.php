33ed73bd73b14413337c53d94196ed472d097ed6
Sambo <kouckengsambo@gmail.com>
2022-08-11 12:40:28 +0700

    Update Autd and Admin pannal

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">TMC School</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">www.troopsmeanchey.com.kh</a>
+            </li>
+        </ul>
+    </div>
+    &copy; 2022/08/11 <a href="#">Koch Kengsambo</a>. All Rights Reserved.
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::middleware([
@@ -23,6 +28,8 @@ Route::middleware([
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.index');
     })->name('dashboard');
 });
+
+Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
